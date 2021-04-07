9f476a6f20f82b2e4e9b792e02dab38e88770071
speraza545 <s.peraza545@gmail.com>
2021-04-06 21:53:26 -0400

    commit tree ef0f4da3df3a6f6b5a70bd7d4d6f22c840965f53

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+    @@pirates = []
+    attr_accessor :name, :weight, :height
+
+    def initialize(args)
+        @name = args[:name]
+        @weight = args[:weight]
+        @height = args[:height]
+        @@pirates << self
+    end
+
+    def self.all 
+        @@pirates
+    end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+    @@ships = []
+    attr_accessor :name, :type, :booty
+
+    def initialize(args)
+        @name = args[:name]
+        @type = args[:type]
+        @booty = args[:booty]
+        @@ships << self
+    end
+
+    def self.all 
+        @@ships
+    end
+
+    def self.clear
+        @@ships = []
+    end
 end
